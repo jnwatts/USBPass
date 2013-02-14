70a5027c4f9e45bed74a28a540fd6203286275c5
Josh Watts <jwatts@d3engineering.com>
2013-02-14 00:17:26 -0500

    Descriptors: Add reminder to measure power usage (not sure exactly how...)

--- sw/embedded/Descriptors.c
+++ sw/embedded/Descriptors.c
@@ -109,7 +109,7 @@ const USB_Descriptor_Configuration_t PROGMEM ConfigurationDescriptor =
 
 			.ConfigAttributes       = USB_CONFIG_ATTR_RESERVED,
 
-			.MaxPowerConsumption    = USB_CONFIG_POWER_MA(100)
+			.MaxPowerConsumption    = USB_CONFIG_POWER_MA(100) //TODO: Determine this!
 		},
 
 	.HID_KeyboardInterface =
