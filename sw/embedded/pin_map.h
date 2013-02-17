3500d94e1b450d092539864698438c7563975c43
Josh Watts <jwatts@d3engineering.com>
2013-02-17 14:19:36 -0500

    PINMAP: Expand button enum to include "long" action

--- sw/embedded/pin_map.h
+++ sw/embedded/pin_map.h
@@ -4,12 +4,15 @@
 #include <avr/io.h>
 
 typedef enum {
-    BUTTON_SW1,
-    BUTTON_SW2,
-    BUTTON_SW3,
+    BUTTON_SW1 = 0x00,
+    BUTTON_SW2 = 0x01,
+    BUTTON_SW3 = 0x02,
+    BUTTON_SW1L = 0x80,
+    BUTTON_SW2L = 0x81,
+    BUTTON_SW3L = 0x82,
     BUTTON_MAX
 } Button_t;
-#define NUM_BUTTONS (BUTTON_MAX)
+#define NUM_BUTTONS (3)
 
 #define SW1_PORT PIND
 #define SW2_PORT PIND
